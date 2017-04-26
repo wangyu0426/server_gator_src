dd527694da5770ce09fd20ff7f1cff6e07628b3c
lwk <liuwenkui_ccnu@hotmail.com>
2017-04-26 18:59:30 +0800

    update

---
+++ proto/tcpporto.go
@@ -0,0 +1,18 @@
+package proto
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"../logger"
+)
+
+func HandleRequest(tcpserverChan chan *MsgData, msg *MsgData)  {
+	imei, _ := strconv.ParseUint(string(msg.Data[0: ImeiLen]), 0, 0)
+	cmd := string(msg.Data[ImeiLen: ImeiLen + CmdLen])
+
+	logger.Log(fmt.Sprintf("imei: %d cmd: %s; go routines: %d", imei, cmd, runtime.NumGoroutine()))
+
+
+	//responseChan <- &MsgData{6,  imei, 0, []byte("Hello!")}
+}
