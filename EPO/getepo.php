bf7e1efc69547e62417e314ddba2e00c43910ca2
dehai <dehai@yahoo.com>
2017-08-01 11:47:44 +0800

    update getepo.php

--- EPO/getepo.php
+++ EPO/getepo.php
@@ -1,7 +1,7 @@
 <?php
-system('rm -rf  36H.EPO; curl -o 36H.EPO  -F username=getepo -F  password=n8vZB9belqO4ydnx  http://service.gatorcn.com/tracker/web/download36h.php?action=down36h');
-if(filesize('36H.EPO') !== 27648){
-	echo "get epo err: " . file_get_contents('36H.EPO') . "\n";
+system('rm -rf  /home/work/server-go-src/EPO/36H.EPO; curl -o /home/work/server-go-src/EPO/36H.EPO  -F username=getepo -F  password=n8vZB9belqO4ydnx  http://service.gatorcn.com/tracker/web/download36h.php?action=down36h');
+if(filesize('/home/work/server-go-src/EPO/36H.EPO') !== 27648){
+	echo "get epo err: " . "\n";
 }else{
 	echo "get epo -- ok\n";
 	system('redis-cli -p 9015 reload epo');
