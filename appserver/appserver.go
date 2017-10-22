6ca791a46d107c4c4e7b267980b61bd37998bd2a
Your Name <you@example.com>
2017-10-22 05:39:24 +0000

    only https

--- appserver/appserver.go
+++ appserver/appserver.go
@@ -105,10 +105,10 @@ func AppServerRunLoop(serverCtx *svrctx.ServerContext)  {
 	http.HandleFunc("/api/gator3-version", GetAppVersionOnline)
 	http.HandleFunc(svrctx.Get().HttpUploadURL, HandleUploadFile)
 
-	http.ListenAndServe(fmt.Sprintf("%s:%d", serverCtx.BindAddr, serverCtx.WSPort), nil)
-	http.ListenAndServeTLS(fmt.Sprintf("%s:%d", serverCtx.BindAddr, serverCtx.WSSPort),
-		"home/ec2-user/work/codes/https_test/watch.gatorcn.com/watch.gatorcn.com.cer",
-		"home/ec2-user/work/codes/https_test/watch.gatorcn.com/watch.gatorcn.com.key",nil)
+	//go http.ListenAndServe(fmt.Sprintf("%s:%d", serverCtx.BindAddr, serverCtx.WSPort), nil)
+	fmt.Println(http.ListenAndServeTLS(fmt.Sprintf("%s:%d", serverCtx.BindAddr, serverCtx.WSSPort),
+		"/home/ec2-user/work/codes/https_test/watch.gatorcn.com/watch.gatorcn.com.cer",
+		"/home/ec2-user/work/codes/https_test/watch.gatorcn.com/watch.gatorcn.com.key",nil))
 }
 
 //for managing connection, 对内负责管理APP连接对象，对外为TCP server提供通信接口
