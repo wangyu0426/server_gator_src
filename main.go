65d5c09d5bf35036be75285c5720c1b2bdf34e5f
steven <chenqwsteven@yeah.net>
2019-01-30 09:02:59 +0800

    main.go

--- main.go
+++ main.go
@@ -6,16 +6,27 @@ import (
 	"./logging"
 	"./gt3tcpserver"
 	"./tcpserver"
+	"./tracetcpserver"
 	"./appserver"
 	"./admin"
 	_ "net/http/pprof"
 	"log"
 	"net/http"
 	"runtime"
+	"os"
+	"syscall"
 )
 
 func main() {
 	monitor.PrintName()
+
+	logFile, err := os.OpenFile("../logs/error.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0660)
+	if err != nil {
+		log.Println("服务启动出错",  "打开异常日志文件失败" , err)
+		return
+	}
+	// 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
+	syscall.Dup2(int(logFile.Fd()), int(os.Stderr.Fd()))
 
 	defer logging.PanicLogAndExit("main: ")
 
@@ -30,6 +41,7 @@ func main() {
 	go gt3tcpserver.TcpServerRunLoop(svrctx.Get())
 	//receive data from watch
 	go tcpserver.TcpServerRunLoop(svrctx.Get())
+	go tracetcpserver.Tracegpsserver(svrctx.Get())
 	//receive data from app
 	go appserver.AppServerRunLoop(svrctx.Get())
 	logging.Log("main start...")
